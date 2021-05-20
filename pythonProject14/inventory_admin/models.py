6eb71e59788f7d0f24fa94918389ecc536698324
afia25 <18201033@uap-bd.edu>
2021-05-20 19:49:40 +0600

    Project 19

--- pythonProject14/inventory_admin/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Inventory_admin(models.Model):
-    inv_admin_id = models.TextField(primary_key=True)
-    inv_admin_name = models.CharField(max_length=50)
-    inv_admin_email = models.CharField(max_length=50)
--- pythonProject14/order_item/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class OrderItemConfig(AppConfig):
-    name = 'order_item'
